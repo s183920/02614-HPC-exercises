df92cc21e94844144469856addce093230518f0d
Lukas Leindals <lukas@leindals.dk>
2023-01-03 13:12:11 +0100

    add day 2 material

---
+++ serial_tuning/day2/tune_labs/distance.c
@@ -0,0 +1,23 @@
+#include "data.h"
+
+#ifdef ARRAY_OF_STRUCTS
+double 
+distance(particle_t *p, int n) {
+    
+    double dist = -99.0;
+    /* fill in your code here
+     *
+     */
+    return dist;
+}
+#else
+double 
+distance(particle_t p, int n) {
+
+    double dist = -99.0;
+    /* fill in your code here
+     *
+     */
+    return dist;
+}
+#endif
