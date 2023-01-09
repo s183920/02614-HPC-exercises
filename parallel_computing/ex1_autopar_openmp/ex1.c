7ea0336da23e2dc0f9935434f3c28861c78fb4b0
Lukas Leindals <s183920@student.dtu.dk>
2023-01-09 16:03:43 +0100

    exercise 1

--- parallel_computing/ex1_autopar_openmp/ex1.c
+++ parallel_computing/ex1_autopar_openmp/ex1.c
@@ -1,30 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+// time
+// #ifdef _OPENMP
+// #include <omp.h>
+// #define mytimer omp_get_wtime
+// #define delta_t(a,b) (1e3 * ((b)-(a)))
+// #else
+// #include <time.h>
+// #define mytimer clock
+// #define delta_t(a,b) (1e3 * ((b) - (a)) / CLOCKS_PER_SEC)
+// #endif
+
+
 
 
 double integrand(double x){
-    return 4.0/(1 + pow(x, 2));
-}
-
-double calc_pi(int N){
-    double pi = 0.0;
-    for(int i = 0; i < N; i++)
-    {
-        double x = (i-0.5)/N;
-        pi += integrand(x);
-    }
-    pi = pi/N;
-    return pi;
+    return 4.0/(1 + x*x);
 }
 
 int main(int argc, char *argv[])
 {
-    // int N = 1000000;
-    int N = atoi(argv[1]);
-    double pi = calc_pi(N);
+    // #ifdef _OPENMP
+    // double t1, t2;
+    // fprintf(stderr, "OpenMP version: timing wallclock time (in ms)!\n");
+    // #else
+    // clock_t t1, t2;
+    // fprintf(stderr, "Serial version: timing CPU time (in ms)!\n");
+    // #endif
 
+    // t1 = mytimer();
+
+    // program
+    double x, sum, h;
+    int N, i;
+
+    sum = 0.0;
+    N = 1000000000;
+    h = 1.0 / (double)N;
+    #pragma omp parallel for default(none) \
+                                shared(N,h) private(i,x) \
+                                reduction(+: sum)
+    for(i = 1; i <= N; i++){
+        x = h * ((double)i -0.5);
+        sum += integrand(x);
+    }// end parallel
+
+    double pi = sum*h;
+    // end program
+
+    // t2 = mytimer();
+    // printf("N = %d\npi = %f\ntime = %f ms\n", N, pi, delta_t(t1, t2));
     printf("N = %d\npi = %f\n", N, pi);
-
     return 0;
 }
+
